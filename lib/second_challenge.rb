e76dfbc74001480ace580fb61b097155590dfdb7
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-25 21:52:03 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,17 +1,20 @@
 def second_challenge
   epic_tragedy = {
+    
     :montague => {
      :patriarch => {},
      :matriarch => {},
      :hero => {},
-     :hero_friends = []
+     :hero_friends => []
       }, 
     :capulet => {
       :patriarch => {},
      :matriarch => {},
      :heroine => {},
-     :heroine_friends = []
-      }}
+     :heroine_friends => []
+      }
+    
+  }
 
   
 end
